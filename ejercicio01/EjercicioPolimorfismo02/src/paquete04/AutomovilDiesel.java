ef4938b0793f7c76823a1b7853b63ff1c3717e16
Daniel Eduardo Flores <danser2002@hotmail.com>
2022-06-27 08:17:23 -0500

    Ejercicio Herencia y Polimorfismo

--- ejercicio01/EjercicioPolimorfismo02/src/paquete04/AutomovilDiesel.java
+++ ejercicio01/EjercicioPolimorfismo02/src/paquete04/AutomovilDiesel.java
@@ -1,14 +1,63 @@
+package paquete04;
+
+import paquete02.Automovil;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package paquete04;
+public class AutomovilDiesel extends Automovil{
+    double numLitros;
+    double costoLitros;
+    double descuentoLitros;
+    
+    public void setCostoLitro(double cl) {
+        costoLitros = cl;
+    }
 
-/**
- *
- * @author reroes
- */
-public class AutomovilDiesel {
+    public void setDescuentoLitros(double dl) {
+        descuentoLitros = dl*numLitros;
+    }
+
+     public void setNumLitros(double nl) {
+        numLitros = nl;
+    }
+    
+    public double getDescuentoLitros() {
+        return descuentoLitros;
+    }
+
+    public double getNumLitros() {
+        return numLitros;
+    }
+    
+    public double getCostoLitro() {
+        return costoLitros;
+    }
+    
+    public double getTotalCancelar(){
+        return valorCancelar;
+    }
+    
+    @Override
+    public void calcularValorCancelar(){
+        valorCancelar = costoLitros *(numLitros - descuentoLitros);
+    }
+
+    @Override 
+    public String toString(){
+        String cadena = String.format("Propietario: %s\n"
+        + "Placa: %s\n" + 
+        "Numéro de galones máximos: %.2f\n"+
+        "Costo por galón: %.2f\nDescuento: %.2f\nValor a cancelar: %.2f\n\n", 
+        nombrePro, 
+        placa, 
+        numLitros, 
+        costoLitros, 
+        descuentoLitros,
+        valorCancelar);
+        return cadena;
+    }
     
 }
